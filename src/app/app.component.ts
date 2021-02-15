d8981b67dcff030d9776cc733f849023e394f908
Mustafa Omran <mustafa.anwer@e-amanah.com>
2021-02-15 23:11:21 +0200

    json forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'json-forms';
+export class AppComponent implements OnInit {
+  data: any;
+  editorOptions: JsonEditorOptions;
+  @ViewChild(JsonEditorComponent, { static: false }) editor: JsonEditorComponent;
+
+  model = {};
+  form = new FormGroup({});
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'input',
+      type: 'input',
+      templateOptions: {
+        label: 'Input',
+        placeholder: 'Input placeholder',
+        required: true,
+      }
+    },
+    {
+      key: 'textarea',
+      type: 'textarea',
+      templateOptions: {
+        label: 'Textarea',
+        placeholder: 'Textarea placeholder',
+        required: true,
+      }
+    },
+    {
+      key: 'checkbox',
+      type: 'checkbox',
+      templateOptions: {
+        label: 'Checkbox',
+      }
+    },
+    {
+      key: 'select',
+      type: 'select',
+      templateOptions: {
+        label: 'Select',
+        placeholder: 'Select placeholder',
+        required: true,
+        options: [
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]
+      }
+    },
+    {
+      key: 'radio',
+      type: 'radio',
+      templateOptions: {
+        label: 'Radio',
+        required: true,
+        options: [
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+        ]
+      }
+    }
+  ];
+
+  constructor() {
+    this.editorOptions = new JsonEditorOptions()
+    this.editorOptions.mode = 'text';
+
+    this.data = {
+      fields: this.fields
+    }
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  getData(event) {
+    this.fields = event.fields;
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      alert(JSON.stringify(this.model, null, 2));
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { NgJsonEditorModule } from 'ang-jsoneditor'
+
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: 'outline'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        { name: 'required', message: 'This field is required' },
+      ],
+    }),
+    FormlyMaterialModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    NgJsonEditorModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: appearance
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
